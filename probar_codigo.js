0233cc85b5462e2c20087f09c4fdbfc27ad4059b
AgustinRC <agusc.reyes@gmail.com>
2023-07-06 21:04:53 -0300

    Boom

---
+++ probar_codigo.js
@@ -0,0 +1,105 @@
+
+// let secuencia =["c","b","a"]
+// let secuencia2=[4,5]
+
+    //secuencia.reverse(); Invierte el orden de los elementos del arreglo
+    // secuencia=secuencia.slice(0,2);  Extrae parte del arreglo, se debe indicar los extremos
+    //  secuencia=secuencia.sort(); ordena de menor a mayor los elementos (si son numero) y de forma alfabetica si son strings
+
+
+    
+
+    // console.log("Los valores son: ")
+    // myCar.color="Negro"
+    // for (var prop in myCar){
+    //     console.log(prop+": "+ myCar[prop])
+
+    // }
+
+    // function recibo(nombre,valor){
+
+    //     let myCar ={}
+    //         myCar.color="Rojo"
+    //         myCar.puertas=2
+    //         myCar["marca_modelo"]="Ford Mustang"
+    //              myCar[nombre]=valor
+        
+    //             console.log(myCar.marca_modelo)
+    // }
+
+    // recibo("ruedas",6)
+
+        // Se crean y asignan cuatro variables de una sola vez,
+// separadas por comas
+// var myObj = new Object(),
+// str = 'myString',
+// rand = Math.random(),
+// obj = new Object();
+
+// myObj.type                 = 'Sintaxis de puntos';
+// myObj['fecha de creación'] = 'Cadena con espacios';
+// myObj[str]                 = 'Valor de cadena';
+// myObj[rand]                = 'Número aleatorio';
+// myObj[obj]                 = 'Object';
+// myObj['']                  = 'Incluso una cadena vacía';
+
+// // console.log(myObj);
+
+// var miAuto={
+//      make:"Ford"
+//  }
+
+// //     let myPrototipo="make"
+
+// //     console.log(miAuto[myPrototipo])
+
+
+//     function pagina(titulo,color,fondo){
+//         this.color=color;
+//         this.fondo=fondo;
+//         this.titulo=titulo;
+//         this.lenght=3;
+//     }
+
+//     var miPagina= new pagina("Mi pagina", "Blue")
+//     var nuevapag= new  pagina("Segunda pagina","White","")
+
+//     pagina.prototype.descripcion="Esto es un ejemplo "
+        
+
+
+//         console.log(miPagina.descripcion)
+//         console.log(nuevapag.descripcion)
+
+
+
+                // class NuevaClase{
+                //     constructor(edad,peso,gernero){
+                //         this.edad=edad
+                //         this.peso=peso
+                //         this.gernero=gernero
+                //     }
+
+                //     calcular_calorias(){
+                //         if(this.gernero=="Femenino"|| this.gernero=="femenino"){
+                //             let calorias=this.peso*2.25
+
+                //                 return(calorias)
+                
+                //         }
+                //         else{
+                //             let calorias=this.peso*3
+                //             return(calorias)
+                //         }
+                //     }
+                // } 
+
+                //     let PrimeraPersona=new NuevaClase(23,69,"Femenino");
+                //     let SegundaPersona=new NuevaClase(20,72,"Masculino")
+
+                // NuevaClase.prototype.actividad="Ninguna";    
+
+                // console.log(PrimeraPersona.actividad)
+                // console.log(SegundaPersona.calcular_calorias())
+    
+        
